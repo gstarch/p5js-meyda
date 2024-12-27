b314257e89c21b967412cac3d35fba3e3e3399f4
gstarch <gstarch@gmail.com>
2024-12-27 17:08:52 +0300

    Move controls to own class

---
+++ Controls.js
@@ -0,0 +1,75 @@
+class Controls {
+  constructor(sound) {
+    this.sound = sound;
+    this.playButton = createButton('Play');
+    this.jumpButton = createButton('Jump');
+    this.volumeSlider = createSlider(0, 1, 0.5, 0.01);
+    this.speedSlider = createSlider(0, 2, 1, 0.01);
+    this.panSlider = createSlider(-1, 1, 0, 0.01);
+
+    this.setupUI();
+  }
+
+  setupUI() {
+    this.playButton.position(10, 10);
+    this.playButton.mousePressed(() => this.playStopSound());
+
+    this.jumpButton.position(70, 10);
+    this.jumpButton.mousePressed(() => this.jumpSound());
+
+    this.volumeSlider.position(10, 40);
+    this.speedSlider.position(10, 80);
+    this.panSlider.position(10, 120);
+
+    fill(255);
+    text('Volume', 53, 75);
+    text('Speed', 53, 115);
+    text('Pan', 53, 155);
+  }
+
+  playStopSound() {
+    if (this.sound.isPlaying()) {
+      console.log('Pausing');
+      this.sound.pause();
+    } else {
+      console.log('Playing');
+      this.sound.play();
+    }
+    this.updatePlayButton();
+  }
+
+  updatePlayButton() {
+    if (this.sound.isPlaying()) {
+      this.playButton.html('Pause');
+    } else {
+      this.playButton.html('Play');
+    }
+  }
+
+  setVolume() {
+    this.sound.setVolume(this.volumeSlider.value());
+  }
+
+  setSpeed() {
+    this.sound.rate(this.speedSlider.value());
+  }
+
+  setPan() {
+    this.sound.pan(this.panSlider.value());
+  }
+
+  jumpSound() {
+    let duration = this.sound.duration();
+    let newPosition = random(0, duration);
+    this.sound.jump(newPosition);
+    console.log('Jumped to', newPosition);
+    console.log('isPlaying:', this.sound.isPlaying());
+    setTimeout(() => this.updatePlayButton(), 100); // avoids play state mismatch on jump
+  }
+
+  update() {
+    this.setVolume();
+    this.setSpeed();
+    this.setPan();
+  }
+} 
--- sketch.js
+++ sketch.js
@@ -1,87 +1,25 @@
 let mySound;
-let playButton;
+let controls;
 let windowWidth = 800;
 let windowHeight = 600;
-let volumeSlider;
-let speedSlider;
-let panSlider;
-let jumpButton;
-
 
 function setup() {
-  soundFormats('wav','mp3');
+  soundFormats('wav', 'mp3');
   mySound = loadSound('../sounds/Kalte Ohren (Remix).mp3');
 
-  
   mySound.onended(() => {
-    playButton.html('Play');
+    controls.playButton.html('Play');
   });
 
   createCanvas(windowWidth, windowHeight);
   background(0);
-  
-  // Play button
-  playButton = createButton('Play');
-  playButton.position(10, 10);
-  playButton.mousePressed(playStopSound);
 
-  // Jump button
-  jumpButton = createButton('Jump');
-  jumpButton.position(70, 10);
-  jumpButton.mousePressed(jumpSound);
-
-  // Volume slider
-  volumeSlider = createSlider(0, 1, 0.5, 0.01);
-  volumeSlider.position(10, 40);
-
-  // Speed slider
-  speedSlider = createSlider(0, 2, 1, 0.01);
-  speedSlider.position(10, 80);
-
-  // Pan slider
-  panSlider = createSlider(-1, 1, 0, 0.01);
-  panSlider.position(10, 120);
-
-  fill(255);
-  text('Volume', 53, 75);
-  text('Speed', 53, 115);
-  text('Pan', 53, 155);
-}
-
-function playStopSound() {
-  if (mySound.isPlaying()) {
-    console.log('Pausing');
-    mySound.pause();
-    playButton.html('Play');
-  } else {
-    console.log('Playing');
-    mySound.play();
-    playButton.html('Pause');
-  }
-}
-
-function setVolume() {
-  mySound.setVolume(volumeSlider.value());
-}
-
-function setSpeed() {
-  mySound.rate(speedSlider.value());
-}
-
-function setPan() {
-  mySound.pan(panSlider.value());
-}
-
-function jumpSound() {
-  let duration = mySound.duration();
-  let newPosition = random(0, duration);
-  mySound.jump(newPosition);
+  // Initialize Controls
+  controls = new Controls(mySound);
 }
 
 function draw() {
   // put drawing code here
-  setVolume();
-  setSpeed();
-  setPan();
+  controls.update();
 }
 
